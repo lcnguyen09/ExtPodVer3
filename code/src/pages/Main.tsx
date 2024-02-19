1a749024871c528c3a43f4d1188a88835fd62904
Le Chi Nguyen <lechinguyen09@gmail.com>
2024-02-19 23:01:07 +0700

    example

--- code/src/pages/Main.tsx
+++ code/src/pages/Main.tsx
@@ -4,14 +4,20 @@ import UiContext from './../contexts/ui.context'
 import NomalItem from "./libs/NomalItem"
 import Inspireuplift from "./libs/inspireuplift/Inspireuplift"
 import Amazon from "./libs/amazon/Amazon"
+import Etsy from "./libs/etsy/Etsy"
 // import EtsySite from "./libs/EtsySite"
 
 export default function Main() {
 	const [site, setSite] = useState(window.location.host)
+	const [pathname, setPathname] = useState(window.location.pathname)
 
 	useEffect(() => {
 		setSite(window.location.host)
 	}, [window.location.host])
+
+	useEffect(() => {
+		setPathname(window.location.pathname)
+	}, [window.location.pathname])
 
 	if (site === 'sellercentral.amazon.com') {
 		return <Amazon />
@@ -19,6 +25,11 @@ export default function Main() {
 
 	if (site === 'sellercentral.inspireuplift.com') {
 		return <Inspireuplift />
+	}
+	if (site === 'www.etsy.com') {
+		if (pathname.startsWith('/your') || true) {
+			return <Etsy />
+		}
 	}
 	return <NomalItem />
 }
